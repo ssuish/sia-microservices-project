f34750acc6e0907a09fb72821a1d23794eb5b3de
suisoh <88411618+ssuish@users.noreply.github.com>
2024-07-04 21:08:56 +0800

    Working view all function.

---
+++ supplier/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Login</title>
+</head>
+
+<body>
+    <section>
+        <div class="row justify-content-center align-items-center my-5">
+            <div class="col-6">
+                <form method="POST" action="includes/login.inc.php">
+                    <div class="mb-3">
+                        <h2 align="center"><label class="form-label">User Log-in</label></h2>
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Username</label>
+                        <input type="text" class="form-control" name="txtUsername" required="true">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="txtPassword" required="true">
+                    </div>
+                    <button type="submit" class="btn btn-primary px-5" name="btnLogin">Submit</button>
+                </form>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
