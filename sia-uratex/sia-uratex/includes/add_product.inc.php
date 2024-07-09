28cd0e7cc64b3e4e4d399554ed968284276a7835
suisoh <88411618+ssuish@users.noreply.github.com>
2024-07-09 09:33:43 +0800

    Supplier monolithic

---
+++ sia-uratex/sia-uratex/includes/add_product.inc.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if (!isset($_SESSION['username']) && !isset($_SESSION['email'])) {
+    header("Location: includes/login.php");
+}
+?>
+
+<?php 
+    require "db.inc.php";
+
+    // btnAddItem onclick
+    if (isset($_POST['btnAddItem'])) {
+        //    post from add item form
+            $name = $_POST['name'];
+            $type = $_POST['type'];
+            $price = $_POST['price'];
+            $size = $_POST['size'];
+            $color = $_POST['color'];
+            $thickness = $_POST['thickness'];
+            $warranty = $_POST['warranty'];
+            $thumbnail = $_POST['thumbnail'];
+            $currency = $_POST['currency'];
+    
+            // query to insert data to tblproducts
+            $conn->query("INSERT INTO `tblproducts`(`name`, `type`, `price`, `size`, `color`, `thickness`, `warranty`, `thumbnail`, `currency`) 
+            VALUES ('$name','$type','$price','$size','$color','$thickness','$warranty','$thumbnail','$currency')");
+
+            // redirect to admin_dashboard.php
+            header("location: ../admin_dashboard.php");
+    }
+?>
